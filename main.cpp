42ef799822aace6df3f7d6d6229ca43dcfaed09b
Hariprasath Krishnamoorthi <hariprasath.krishnamoorthi@psiog.com>
2025-08-17 13:21:48 -0700

    Second  commit

--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@ void incomingOrder() {
     cout << "Incoming order\n";
 }
 
+bool pickupTime(int &minutesToPickup) {
+    if (minutesToPickup <= 30) {
+        cout << "Pickup within 30 minutes\n";
+        return true;
+    } else {
+        cout << "Waiting for pickup time\n";
+        this_thread::sleep_for(chrono::seconds(1));
+        minutesToPickup--;
+        return false;
+    }
+}
+
 
 int main() {
     int minutesToPickup = 35;
